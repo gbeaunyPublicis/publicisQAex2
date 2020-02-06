c15465c87569358eb8f5d80d7519cafa5363f4f3
Geoffrey <geoffrey.beauny@publicissapient.com>
2020-02-06 16:45:09 +0100

    test: ex2 e2E testing add files

---
+++ test/e2e/venteprivee.js
@@ -0,0 +1,74 @@
+var conf = require('../../nightwatch.conf.js');
+
+module.exports = {
+  'Open Website Vente privée': function (browser) {
+    browser
+      
+  },
+
+  'Check block login': function (browser) {
+    browser
+      
+  },
+
+  'Open registration page': function (browser) {
+    browser
+      
+  },
+
+  'RadioButton homme must exists': function (browser) {
+    browser
+      
+  },
+
+  'RadioButton femme must exists': function (browser) {
+    browser
+      
+  },
+
+  'RadioButton femme must exists': function (browser) {
+    browser
+      
+  },
+
+  'Field "Nom" must exists': function (browser) {
+    browser
+      
+  },
+
+  'Field "Prenom" must exists': function (browser) {
+    browser
+      
+  },
+
+  'Field "Email" must exists': function (browser) {
+    browser
+      
+  },
+
+  'Field "Mot De Passe" must exists': function (browser) {
+    browser
+      
+  },
+  
+  'Checkbox Legal aggrement must exist': function (browser) {
+    browser
+      
+  },
+
+  'Button C\'est Parti must exist': function (browser) {
+    browser
+      
+  },
+
+  'Registration shouldn\t pass': function (browser) {
+    browser
+      
+  },
+
+  'Registration should pass': function (browser) {
+    browser
+
+    browser.end();
+  }
+}
